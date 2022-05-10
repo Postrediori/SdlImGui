3f719582be29e21c7c71f1871477b91fb99a52e2
Rasim Labibov <postrediori@gmail.com>
2022-05-10 19:28:43 +0300

    Update ImGui CMake project file

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -1,20 +1,37 @@
 set(IMGUI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/3rdparty/imgui)
 file(GLOB IMGUI_SOURCES ${IMGUI_INCLUDE_DIR}/*.cpp)
 file(GLOB IMGUI_HEADERS ${IMGUI_INCLUDE_DIR}/*.h)
-                 
-add_library(imgui STATIC ${IMGUI_SOURCES} ${IMGUI_SOURCES})
 
-add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+set(IMGUI_BACKENDS_DIR ${IMGUI_INCLUDE_DIR}/backends)
+set(IMGUI_BACKEND_SOURCES
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl2.cpp
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl3.cpp
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_sdl.cpp
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_sdlrenderer.cpp)
+set(IMGUI_BACKEND_HEADERS
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl2.h
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl3.h
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_sdl.h
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_sdlrenderer.h)
 
-include_directories(
+add_library(imgui STATIC
+    ${IMGUI_SOURCES} ${IMGUI_SOURCES}
+    ${IMGUI_BACKEND_HEADERS} ${IMGUI_BACKEND_SOURCES})
+
+target_compile_definitions(imgui PUBLIC IMGUI_IMPL_OPENGL_LOADER_GLAD)
+
+target_include_directories(imgui PUBLIC
     ${IMGUI_INCLUDE_DIR}
+    ${IMGUI_BACKENDS_DIR}
     ${OPENGL_INCLUDE_DIR}
-    ${SDL2_INCLUDE_DIR})
-    
+    ${SDL2_INCLUDE_DIRS}
+    ${GLAD_INCLUDE_DIR})
+
 target_link_libraries(imgui
     ${OPENGL_LIBRARIES}
-    ${SDL2_LIBRARIES})
-    
+    ${SDL2_LIBRARIES}
+    ${GLAD_LIBRARIES})
+
 set_target_properties(imgui PROPERTIES LINKER_LANGUAGE CXX)
 set_target_properties(imgui PROPERTIES FOLDER 3rdparty)
 
